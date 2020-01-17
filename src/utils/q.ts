e5195317ad85d53c4c0d5200f61faeace72ae17f
jw397 <jw397@126.com>
2020-01-17 11:32:22 +0800

    feat(utils): q

---
+++ src/utils/q.ts
@@ -0,0 +1,26 @@
+/**
+ * Q的接口
+ */
+const Q: any = {};
+Q.defer = function () {
+  let _resolve, _reject;
+  const promise = new Promise(function (resolve, reject) {
+    _resolve = resolve;
+    _reject = reject;
+  });
+  return {
+    resolve: _resolve,
+    reject: _reject,
+    promise: promise
+  };
+};
+Q.reject = Promise.reject;
+Q.resolve = Promise.resolve;
+Q.all = Promise.all;
+// obj.each = Promise.each;
+// obj.join = Promise.join;
+Q.isPromise = function (promise: any) {
+  return promise instanceof Promise;
+};
+
+export default Q;
