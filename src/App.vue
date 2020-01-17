450f1ab6e43a0dcddefa4ef7037e5ba78d83306d
jw397 <jw397@126.com>
2020-01-17 16:15:42 +0800

    style(lint): 使用ts

--- src/App.vue
+++ src/App.vue
@@ -27,101 +27,108 @@
     </Tabs>
   </div>
 </template>
-<script>
-  import {types} from "./enum";
-  import Basic from './views/system/Basic.vue';
-  import Resource from './views/system/Resource.vue';
-  import Selector from './views/system/Selector.vue';
+<script lang="ts">
+import Vue from 'vue';
+import {types} from './enum';
+import Basic from './views/system/Basic.vue';
+import Resource from './views/system/Resource.vue';
+import Selector from './views/system/Selector.vue';
 
+interface IChild {
+  name: string;
+  caption: string;
+}
 
-  export default {
-    data () {
-      const data = [
-        {
-          name: 'system',
-          caption: '系统',
-          children: [{
-            name: 'basic',
-            caption: '基本'
-          }, {
-            name: 'resource',
-            caption: '资源'
-          }, {
-            name: 'selector',
-            caption: '选择器'
-          }]
-        },
-        {
-          name: 'func',
-          caption: '系统',
-          children: [{
-            name: 'layer',
-            caption: '图层对象'
-          }, {
-            name: 'menu',
-            caption: '面板'
-          }, {
-            name: 'monitor',
-            caption: '监控报警'
-          }]
-        },
-        {
-          name: 'exhibition',
-          caption: '展示',
-          children: [{
-            name: 'viewpoint',
-            caption: '视角'
-          }, {
-            name: '3d',
-            caption: '3D效果'
-          }, {
-            name: 'alarm-color',
-            caption: '告警颜色'
-          }]
-        },
-      ];
-      return {
-        curTab: '',
-        current: '',
-        curTabData: {},
-        data
-      };
+export default Vue.extend({
+  data() {
+    const data = [
+      {
+        name: 'system',
+        caption: '系统',
+        children: [{
+          name: 'basic',
+          caption: '基本'
+        }, {
+          name: 'resource',
+          caption: '资源'
+        }, {
+          name: 'selector',
+          caption: '选择器'
+        }]
+      },
+      {
+        name: 'func',
+        caption: '系统',
+        children: [{
+          name: 'layer',
+          caption: '图层对象'
+        }, {
+          name: 'menu',
+          caption: '面板'
+        }, {
+          name: 'monitor',
+          caption: '监控报警'
+        }]
+      },
+      {
+        name: 'exhibition',
+        caption: '展示',
+        children: [{
+          name: 'viewpoint',
+          caption: '视角'
+        }, {
+          name: '3d',
+          caption: '3D效果'
+        }, {
+          name: 'alarm-color',
+          caption: '告警颜色'
+        }]
+      }
+    ];
+    return {
+      curTab: '',
+      curTabData: [] as IChild[],
+      data
+    };
+  },
+  components: {
+    Basic,
+    Resource,
+    Selector
+  },
+  methods: {
+    changeCurData(name: string) {
+      let index = this.data.findIndex((item) => {
+        return item.name === name;
+      });
+      if (index !== -1) {
+        this.changeCurDataByIndex(index);
+      }
     },
-    components: {
-      Basic,
-      Resource,
-      Selector
+    changeCurDataByIndex(index: number) {
+      const curTabData = this.data[index].children;
+      if (!curTabData || curTabData.length === 0) {
+        return;
+      }
+      this.curTabData = curTabData;
+      this.curTab = curTabData[0].name;
+      (this.$refs.tabs as any).activeKey = this.curTab; // 激活第1个tab页
     },
-    methods: {
-      changeCurData (name) {
-        let index = this.data.findIndex((item) => {
-          return item.name === name;
-        });
-        if (index != -1) {
-          this.changeCurDataByIndex(index);
-        }
-      },
-      changeCurDataByIndex (index) {
-        this.curTabData = this.data[index].children;
-        this.curTab = this.curTabData.name;
-        this.current = this.curTabData[0].name;
-        this.$refs.tabs.activeKey = this.current;//激活第1个tab页
-      },
-      change (name) {
-        this.$store.commit(types.SET_CURRENT_TAB, name);
-      },
-      save () {
-        // this[types.SAVE]({name:'jw',val:123});
-        // this.$store.commit(types.SAVE, {name:'jw',val:123});
-        this.$store.dispatch(types.SAVE);
-      },
-      // ...mapMutations([types.SAVE])
+    change(name: string) {
+      this.$store.commit(types.SET_CURRENT_TAB, name);
     },
-    mounted () {
-      this.changeCurDataByIndex(0);
-      this.change(this.current);
+    save() {
+      // this[types.SAVE]({name:'jw',val:123});
+      // this.$store.commit(types.SAVE, {name:'jw',val:123});
+      this.$store.dispatch(types.SAVE);
     }
+    // ...mapMutations([types.SAVE])
+  },
+  mounted() {
+    this.changeCurDataByIndex(0);
+    this.change(this.curTab);
   }
+});
 </script>
 
 <style>
--- src/views/system/Selector.vue
+++ src/views/system/Selector.vue
@@ -41,7 +41,7 @@
 
   export default {
     components: {
-      myList: myList
+      myList
     },
     data() {
       return {
