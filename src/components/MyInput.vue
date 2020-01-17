6783d0b6575dc25cb05ef3797ac8968a518ec81e
jw397 <jw397@126.com>
2020-01-17 11:51:22 +0800

    feat(input): 使用ts

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -9,57 +9,21 @@
 
 </template>
 
-<script>
-  export default {
-    name: 'MyInput',
-    props: {
-      props: Object
-      // {
-      //   name: String,
-      //   caption: String,
-      //   type: String,
-      //   defaultValue: [String, Number]
-      // }
-    },
-    methods: {
-      // blur (e) {
-      // if (this.type !== 'number') {
-      //   return;
-      // }
-      // const event = e || window.event;
-      // let value = event.target.value;
-      // const reg = /^[+-]?(0|([1-9]\d*))(\.\d+)?$/g;
-      // if (!reg.test(value)) {
-      //   value = value.replace(/[^0-9.]/g, '');
-      //   event.target.value = value;
-      // }
-      // },
-      // keypress (e) {
-      // if (this.type !== 'number') {
-      //   return;
-      // }
-      // const event = e || window.event;
-      // const val = event.target.value;
-      // //控制第一个不能输入小数点"."
-      // if (val.length === 0 && event.which === 46) {
-      //   event.preventDefault();
-      //   return;
-      // }
-      // //控制只能输入一个小数点"."
-      // if (val.indexOf('.') !== -1 && event.which === 46) {
-      //   event.preventDefault();
-      //   return;
-      // }
-      // //控制只能输入的值
-      // if (event.which && (event.which < 48 || event.which > 57) && event.which !== 8 && event.which !== 46) {
-      //   event.preventDefault();
-      // }
-      // }
-    }
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  interface IInput {
+    name: string;
+    caption: string;
+    defaultValue: string | number;
+    type: string;
   }
 
+  @Component
+  export default class MyInput extends Vue {
+    @Prop() private props: IInput;
+  }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus">
 
